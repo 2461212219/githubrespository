20b6348ac56fad360431e1cf51e78311583c6d27
admin <admin@qq.com>
2020-11-20 22:12:40 +0800

    demo09第四次提交

--- GitDemo09/src/com/gec/test/Test1.java
+++ GitDemo09/src/com/gec/test/Test1.java
@@ -13,6 +13,8 @@ public class Test1 {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
 
     }
 
