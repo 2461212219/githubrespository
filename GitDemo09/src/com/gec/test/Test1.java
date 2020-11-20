31493d21451f0f12a8a84fad5a74de4096fb16b9
admin <admin@qq.com>
2020-11-20 21:35:02 +0800

    demo09

---
+++ GitDemo09/src/com/gec/test/Test1.java
@@ -0,0 +1,17 @@
+package com.gec.test;
+
+/**
+ * 功能描述
+ *
+ * @author 薛创中
+ * @version V1.0
+ * @date 2020/11/20 0020
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("111111111111");
+
+    }
+
+}
