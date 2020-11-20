04e8f441837ec1c6a163e2ca97f650a14f9bbd60
admin <admin@qq.com>
2020-11-20 21:58:18 +0800

    demo09第二次提交

--- GitDemo09/src/com/gec/test/Test1.java
+++ GitDemo09/src/com/gec/test/Test1.java
@@ -9,8 +9,10 @@ package com.gec.test;
  */
 public class Test1 {
     public static void main(String[] args) {
-
-        System.out.println("111111111111");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
 
     }
 
